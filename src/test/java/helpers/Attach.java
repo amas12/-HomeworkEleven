5354400954df1b92b7e1640a0f0f84c7ea9ba6b9
Albina <amasgutova7@gmail.com>
2022-02-14 21:40:18 +0300

    added Attachment

--- src/test/java/helpers/Attach.java
+++ src/test/java/helpers/Attach.java
@@ -55,4 +55,7 @@ public class Attach {
         return null;
     }
 
+    public static String getSessionId(){
+        return ((RemoteWebDriver) getWebDriver()).getSessionId().toString();
+    }
 }
